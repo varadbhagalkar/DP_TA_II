fd1234e8a59a62f2251a055b7364bf1fa79310f9
viftergr <varadbhagalkar@gmail.com>
2022-11-25 21:57:20 +0530

    Initial Commit

---
+++ src/main/java/org/example/EmployeeList.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class EmployeeList {
+    //singleton for employee
+}
---
+++ src/main/java/org/example/EmployeeLogin.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class EmployeeLogin {
+    //proxy for checking employee
+    //options to apply for leave,appraisal,resign
+}
